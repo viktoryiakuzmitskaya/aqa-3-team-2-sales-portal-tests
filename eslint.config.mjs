28adb94f597e71c45df71125e4dc533fefccdb44
Viktoryia Kuzmitskaya <kuzmitskaya.viktoryia@gmail.com>
2025-06-11 00:46:43 +0700

    Installed prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,22 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: pluginPrettier
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+      ]
+    }
+  },
+  prettier,
   {
     ignores: ["dist", "node_modules", "eslint.config.mjs", "eslint.config.mjs"]
   }
