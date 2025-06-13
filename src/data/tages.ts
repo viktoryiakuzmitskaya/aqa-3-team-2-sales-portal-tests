96768b96446c1ff1af2b15862643e693e0f97054
angelina-yp <angelinyshechka@mail.ru>
2025-06-13 10:46:57 +0300

    add tages

---
+++ src/data/tages.ts
@@ -0,0 +1,9 @@
+export enum TAGS {
+  SMOKE = '@smoke',
+  REGRESSION = '@regression',
+  UI = '@ui',
+  API = '@api',
+  VISUAL_REGRESSION = '@visual_regression',
+  PRODUCTS = '@Products',
+  CUSTOMERS = '@Customers',
+}
