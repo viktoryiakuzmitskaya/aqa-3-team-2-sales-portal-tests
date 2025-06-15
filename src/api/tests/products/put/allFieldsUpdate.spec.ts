b75528b4e8d0ee93ddb94335b1f12d1abfb64a53
Viktoryia Kuzmitskaya <43791662+viktoryiakuzmitskaya@users.noreply.github.com>
2025-06-15 19:46:43 +0700

    Update and rename fullValidUpdate.spec.ts to allFieldsUpdate.spec.ts

--- src/api/tests/products/put/allFieldsUpdate.spec.ts
+++ src/api/tests/products/put/allFieldsUpdate.spec.ts
@@ -1,20 +1,22 @@
 import { test, expect } from 'fixtures/index';
-import { generateProductData } from 'data/products/generateProduct.data';
-import { validateSchema } from 'utils/notifications/validations/schemaValidation';
-import { validateResponse } from 'utils/notifications/validations/responseValidation';
+import { IProduct } from 'types/products.types';
 import { productSchema } from 'data/schemas/products/product.schema';
 import { STATUS_CODES } from 'data/status.code';
 import { MANUFACTURERS } from 'data/products/manufacturers.data';
-import { IProduct } from 'types/products.types';
 import { TAGS } from 'data/tags';
+import { generateProductData } from 'data/products/generateProduct.data';
+import { validateSchema } from 'utils/notifications/validations/schemaValidation';
+import { validateResponse } from 'utils/notifications/validations/responseValidation';
 
+let token = '';
 const initialProductData = generateProductData({ manufacturer: MANUFACTURERS.XIAOMI });
-let productId: string;
+let productId = '';
 
-test.beforeEach(async ({ productService, validToken }) => {
+test.beforeEach(async ({ signInService, productService }) => {
+  token = await signInService.loginAsLocalUser();
   const createProductResponse = await productService.controller.create(
     initialProductData,
-    validToken,
+    token,
   );
   validateResponse(createProductResponse, STATUS_CODES.CREATED, true, null);
   validateSchema(productSchema, createProductResponse.body);
@@ -22,8 +24,7 @@ test.beforeEach(async ({ productService, validToken }) => {
 });
 
 test(`${TAGS.API} ${TAGS.PRODUCTS} should update all fields of the product`, async ({
-  productService,
-  validToken,
+  productService
 }) => {
   const updatedProductData: IProduct = {
     ...initialProductData,
@@ -36,16 +37,16 @@ test(`${TAGS.API} ${TAGS.PRODUCTS} should update all fields of the product`, asy
   const updateProductResponse = await productService.controller.updateById(
     updatedProductData,
     productId,
-    validToken,
+    token,
   );
   validateResponse(updateProductResponse, STATUS_CODES.OK, true, null);
   validateSchema(productSchema, updateProductResponse.body);
   expect.soft(updateProductResponse.body.Product).toMatchObject({ ...updatedProductData });
 });
 
-test.afterEach(async ({ productService, validToken }) => {
+test.afterEach(async ({ productService }) => {
   if (productId) {
-    const deleteResponse = await productService.controller.delete(productId, validToken);
+    const deleteResponse = await productService.controller.delete(productId, token);
     expect.soft(deleteResponse.status).toBe(STATUS_CODES.DELETED);
   }
 });
