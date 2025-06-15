ffb28118700213a637b2d8ffbfb8a79966dfb35d
Viktoryia Kuzmitskaya <43791662+viktoryiakuzmitskaya@users.noreply.github.com>
2025-06-15 19:47:13 +0700

    Update index.ts

--- src/fixtures/index.ts
+++ src/fixtures/index.ts
@@ -1,8 +1,7 @@
 import { test as controllers } from 'fixtures/controllers.fixtures';
 import { test as services } from 'fixtures/services.fixtures';
-import { test as validToken } from 'fixtures/validToken.fixture';
 import { expect, mergeTests } from '@playwright/test';
 
-const test = mergeTests(services, controllers, validToken);
+const test = mergeTests(services, controllers);
 
 export { expect, test };
