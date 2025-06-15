5f3e478ae156b53c96ca5c65053a55b9da78a75f
Viktoryia Kuzmitskaya <43791662+viktoryiakuzmitskaya@users.noreply.github.com>
2025-06-15 22:43:35 +0700

    Update validNameUpdate.spec.ts

--- src/api/tests/products/put/validNameUpdate.spec.ts
+++ src/api/tests/products/put/validNameUpdate.spec.ts
@@ -27,7 +27,7 @@ test.beforeEach(async ({ signInService, productService }) => {
 const nameLengths = [3, 40];
 
 for (const length of nameLengths) {
-  test(`${TAGS.API} ${TAGS.PRODUCTS} should update product name (${length} characters)`, async ({
+  test(`${TAGS.API} ${TAGS.PRODUCTS} should update product name with ${length} characters`, async ({
     productService
   }) => {
     const updatedProductData: IProduct = {
