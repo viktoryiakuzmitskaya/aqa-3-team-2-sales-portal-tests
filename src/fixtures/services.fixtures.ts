37bc0191ac2ee2530408c32cc1ece5b23ba88b5c
Dzmitry Litvinovich <dmitrylitvinovich@gmail.com>
2025-06-17 15:50:36 +0300

    add notification in fixtures

--- src/fixtures/services.fixtures.ts
+++ src/fixtures/services.fixtures.ts
@@ -2,12 +2,13 @@ import { test as base } from '@playwright/test';
 import { CustomersApiService } from '../api/services/customers.api-service';
 import { ProductsApiService } from 'api/services/products.api-service';
 import { SignInApiService } from 'api/services/signIn.api-service';
+import { NotificationsApiService } from 'api/services/notifications.api-service';
 
 interface IServiceFixtures {
   customerService: CustomersApiService;
   productService: ProductsApiService;
   signInService: SignInApiService;
-  product: IProductFromResponse;
+  notificationService: NotificationsApiService;
 }
 
 export const test = base.extend<IServiceFixtures>({
@@ -21,15 +22,8 @@ export const test = base.extend<IServiceFixtures>({
   signInService: async ({ request }, use) => {
     await use(new SignInApiService(request));
   },
-  product: async ({ productService, signInService }, use) => {
-    const token = await signInService.loginAsLocalUser();
-    const product = await productService.create(token);
-    await use(product);
-    try {
-      await productService.controller.delete(product._id, token);
-    } catch (error) {
-      console.error(`Failed to delete product ${product._id}:`, error);
-    }
+  notificationService: async ({ request }, use) => {
+    await use(new NotificationsApiService(request));
   },
 });
 
