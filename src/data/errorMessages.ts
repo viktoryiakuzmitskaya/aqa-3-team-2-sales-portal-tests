a38407af150dee55f908311a764307397f8083d0
nmvalx1 <naumov.alex00@icloud.com>
2025-06-19 23:20:52 +0300

    products getAll getById Delete

--- src/data/errorMessages.ts
+++ src/data/errorMessages.ts
@@ -5,5 +5,6 @@ export const ERRORS = {
   NOT_AUTHORIZED: 'Not authorized',
   INCORRECT_REQUEST_BODY: 'Incorrect request body',
   CUSTOMER_NOT_FOUND: (id: string) => `Customer with id '${id}' wasn't found`,
+  PRODUCT_NOT_FOUND: (id: string) => `Product with id '${id}' wasn't found`,
   CUSTOMER_ALREADY_EXISTS: (email: string) => `Customer with email '${email}' already exists`,
 } as const;
